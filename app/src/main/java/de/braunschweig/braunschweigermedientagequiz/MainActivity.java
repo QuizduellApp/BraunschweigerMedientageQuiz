4501f7ca81c4fc53451beb6560088e0b3849f889
Iwan <orion@dhcp180-235.wlan.rz.tu-bs.de>
2014-11-20 15:16:57 +0100

    Revert "Revert 13cac39..87f6742"
    This reverts commit 3ff467b1fdd6c1d5564356fa69e80ff5f0e5ab1f.

--- app/src/main/java/de/braunschweig/braunschweigermedientagequiz/MainActivity.java
+++ app/src/main/java/de/braunschweig/braunschweigermedientagequiz/MainActivity.java
@@ -1,10 +1,13 @@
 package de.braunschweig.braunschweigermedientagequiz;
 
+
+import android.content.Intent;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -12,6 +15,17 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /** Registration Button*/
+        Button registrieren = (Button) findViewById(R.id.buttonNeuerBenutzer);
+        registrieren.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(),
+                        Registrieren.class);
+                startActivityForResult(myIntent, 0);
+            }
+
+        });
+
     }
 
 
