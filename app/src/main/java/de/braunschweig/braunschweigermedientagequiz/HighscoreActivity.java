f3fcaa2c87e031bc88141618787c179917bb6493
Iwan <orion@dhcp178-044.wlan.rz.tu-bs.de>
2015-01-20 13:45:35 +0100

    Kleinere Design Änderungen

--- app/src/main/java/de/braunschweig/braunschweigermedientagequiz/HighscoreActivity.java
+++ app/src/main/java/de/braunschweig/braunschweigermedientagequiz/HighscoreActivity.java
@@ -1,12 +1,12 @@
 package de.braunschweig.braunschweigermedientagequiz;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class HighscoreActivity extends ActionBarActivity {
+public class HighscoreActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
