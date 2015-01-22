5904c28da1b857982f5e6ae49b3e5807b1d39d41
Trinto <dweber@tu-bs.de>
2015-01-22 22:32:49 +0100

    Design

--- app/src/main/java/de/braunschweig/braunschweigermedientagequiz/NeuesSpielActivity.java
+++ app/src/main/java/de/braunschweig/braunschweigermedientagequiz/NeuesSpielActivity.java
@@ -1,11 +1,13 @@
 package de.braunschweig.braunschweigermedientagequiz;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
 import java.util.Map;
